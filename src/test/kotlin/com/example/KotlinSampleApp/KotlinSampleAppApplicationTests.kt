3e5229e61b73f511bdc13d1421d21160748fd666
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-05-01 20:25:25 +0900

    initialize project

---
+++ src/test/kotlin/com/example/KotlinSampleApp/KotlinSampleAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.KotlinSampleApp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSampleAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
