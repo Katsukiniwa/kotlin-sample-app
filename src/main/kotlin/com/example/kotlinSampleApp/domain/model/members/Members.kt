8ee286b03575e6c51a58f960a7122ec2d0cd9889
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-12-12 11:59:21 +0900

    define secretary type

---
+++ src/main/kotlin/com/example/kotlinSampleApp/domain/model/members/Members.kt
@@ -0,0 +1,4 @@
+package com.example.kotlinSampleApp.domain.model.members
+
+class Members {
+}
---
+++ src/main/kotlin/com/example/kotlinSampleApp/domain/model/members/SecretaryType.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinSampleApp.domain.model.members
+
+/**
+ * 幹事区分
+ */
+enum class SecretaryType {
+    /**
+     * 幹事以外
+     */
+    NonSecretary,
+
+    /**
+     * 幹事
+     */
+    Secretary
+}
