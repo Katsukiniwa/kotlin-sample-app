d3a220191f0a0202c025150ea36540001dcec963
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-03-29 00:32:42 +0900

    create party

--- src/main/kotlin/com/example/kotlinSampleApp/domain/model/party/Party.kt
+++ src/main/kotlin/com/example/kotlinSampleApp/domain/model/party/Party.kt
@@ -1,4 +1,16 @@
 package com.example.kotlinSampleApp.domain.model.party
 
+import com.example.kotlinSampleApp.domain.model.amount.types.MemberPaymentTypes
+import com.example.kotlinSampleApp.domain.model.members.Members
+import java.util.*
+
 class Party {
+//    private val name: PartyName
+
+    private constructor(
+            name: PartyName,
+            members: Members,
+            memberPaymentTypes: MemberPaymentTypes,
+//            differenceAmountAdjustmentType: DifferenceAmountAdjustmentType
+    )
 }
