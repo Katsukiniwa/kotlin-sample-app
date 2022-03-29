2304553ef2fc6d9330e901cb3d54d7ce9fd5bb5c
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-03-30 07:14:28 +0900

    update MemberPaymentType

--- src/main/kotlin/com/example/kotlinSampleApp/domain/model/amount/types/MemberPaymentType.kt
+++ src/main/kotlin/com/example/kotlinSampleApp/domain/model/amount/types/MemberPaymentType.kt
@@ -3,5 +3,29 @@ package com.example.kotlinSampleApp.domain.model.amount.types
 import com.example.kotlinSampleApp.domain.model.members.Member
 import com.example.kotlinSampleApp.domain.model.payment_type.PaymentType
 
-class MemberPaymentType private constructor (){
+/**
+ * 参加者支払区分。
+ *
+ * <p>参加者に設定された支払い区分を表す。
+ */
+class MemberPaymentType private constructor(private val member: Member, private val paymentType: PaymentType) {
+    companion object {
+        /**
+         * ファクトリメソッド。
+         *
+         * @param member {@link Member}
+         * @param paymentType {@link PaymentType}
+         */
+        fun of(member: Member, paymentType: PaymentType): MemberPaymentType {
+            return MemberPaymentType(member, paymentType)
+        }
+    }
+
+    fun member(): Member {
+        return this.member
+    }
+
+    fun paymentType(): PaymentType {
+        return this.paymentType
+    }
 }
