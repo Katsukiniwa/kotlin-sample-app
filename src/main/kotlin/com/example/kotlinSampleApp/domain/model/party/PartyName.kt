7e80e2bad0ddb71587dae917ac108c98c9a42a66
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-03-30 07:14:40 +0900

    update party name value object

--- src/main/kotlin/com/example/kotlinSampleApp/domain/model/party/PartyName.kt
+++ src/main/kotlin/com/example/kotlinSampleApp/domain/model/party/PartyName.kt
@@ -1,4 +1,24 @@
 package com.example.kotlinSampleApp.domain.model.party
 
-class PartyName {
+import java.lang.IllegalArgumentException
+
+data class PartyName constructor(private val value: String) {
+
+    init {
+        if (value.isEmpty()) {
+            throw IllegalArgumentException("value is empty")
+        }
+    }
+
+    companion object {
+        /**
+         * ファクトリメソッド。
+         *
+         * @param value 飲み会名
+         * @return {@link PartyName}
+         */
+        fun of(value: String): PartyName {
+            return PartyName(value)
+        }
+    }
 }
