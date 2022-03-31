86c95ddb8b549d3b8bb967b7a50948623214df8b
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-03-31 13:22:09 +0900

    add rate files

---
+++ src/main/kotlin/com/example/kotlinSampleApp/domain/model/amount/rate/AmountRate.kt
@@ -0,0 +1,9 @@
+package com.example.kotlinSampleApp.domain.model.amount.rate
+
+/**
+ * 料金割合。
+ *
+ * <p>料金の割合を表す値オブジェクト。
+ */
+class AmountRate {
+}
---
+++ src/main/kotlin/com/example/kotlinSampleApp/domain/model/amount/rate/PaymentTypeAmountRates.kt
@@ -0,0 +1,4 @@
+package com.example.kotlinSampleApp.domain.model.amount.rate
+
+class PaymentTypeAmountRates {
+}
