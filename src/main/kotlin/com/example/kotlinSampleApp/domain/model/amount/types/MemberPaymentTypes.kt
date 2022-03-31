faff44b4785d3f658cb34c19340002764d1dab90
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-03-31 13:10:48 +0900

    create MemberPaymentTypes

--- src/main/kotlin/com/example/kotlinSampleApp/domain/model/amount/types/MemberPaymentTypes.kt
+++ src/main/kotlin/com/example/kotlinSampleApp/domain/model/amount/types/MemberPaymentTypes.kt
@@ -1,10 +1,17 @@
 package com.example.kotlinSampleApp.domain.model.amount.types
 
+
 /** 参加者支払区分リスト */
 class MemberPaymentTypes {
     private val values: List<MemberPaymentType>;
 
     private constructor(values: List<MemberPaymentType>){
         this.values = values
+    }
+
+    companion object {
+        fun of(values: List<MemberPaymentType>): MemberPaymentTypes? {
+            return MemberPaymentTypes(values)
+        }
     }
 }
