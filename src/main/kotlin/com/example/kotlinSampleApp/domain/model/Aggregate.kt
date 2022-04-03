52e4a1318bbcccb27d20d43138980648f21af05e
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-04-03 18:44:33 +0900

    create aggregate

--- src/main/kotlin/com/example/kotlinSampleApp/domain/model/Aggregate.kt
+++ src/main/kotlin/com/example/kotlinSampleApp/domain/model/Aggregate.kt
@@ -1,4 +1,15 @@
 package com.example.kotlinSampleApp.domain.model
 
-class Aggregate {
+import com.example.kotlinSampleApp.domain.event.DomainEvent
+
+abstract class Aggregate: Entity() {
+    abstract val domainEvents: List<DomainEvent>
+
+    fun occur(event: DomainEvent) {
+        this.domainEvents.plus(event)
+    }
+
+    fun removeEvent(event: DomainEvent) {
+        this.domainEvents.filter { it != event }
+    }
 }
