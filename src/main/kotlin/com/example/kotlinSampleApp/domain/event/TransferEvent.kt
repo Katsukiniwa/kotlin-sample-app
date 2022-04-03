d48bc622ee4cb5ec5871233d6e0873f0b38808d8
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-04-03 18:44:45 +0900

    create transfer event

--- src/main/kotlin/com/example/kotlinSampleApp/domain/event/TransferEvent.kt
+++ src/main/kotlin/com/example/kotlinSampleApp/domain/event/TransferEvent.kt
@@ -1,4 +1,19 @@
 package com.example.kotlinSampleApp.domain.event
 
-class TransferEvent {
+import java.lang.Exception
+import java.util.*
+
+/**
+ * ユーザ間で送金が行われた時にディスパッチされるドメインイベント
+ */
+data class TransferEvent(
+        val fromUserId: UUID,
+        val toUserId: UUID,
+        val amount: Int,
+) : DomainEvent {
+    init {
+        if (amount <= 0) {
+            throw Exception("amount must be positive")
+        }
+    }
 }
