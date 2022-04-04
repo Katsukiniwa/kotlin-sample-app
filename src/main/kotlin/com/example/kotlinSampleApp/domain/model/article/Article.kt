a2e0c00ee2e89afc2928426d3be3fb3ebabc5c5d
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-04-04 21:08:36 +0900

    update article

--- src/main/kotlin/com/example/kotlinSampleApp/domain/model/article/Article.kt
+++ src/main/kotlin/com/example/kotlinSampleApp/domain/model/article/Article.kt
@@ -1,4 +1,68 @@
 package com.example.kotlinSampleApp.domain.model.article
 
-class Article {
+import com.example.kotlinSampleApp.domain.event.DomainEvent
+import com.example.kotlinSampleApp.domain.model.comment.Comment
+import com.example.kotlinSampleApp.domain.model.Aggregate
+import java.time.LocalDate
+import java.util.*
+
+class Article private constructor(
+        id: UUID,
+        title: String,
+        content: String,
+        comments: List<Comment>,
+        publishedAt: LocalDate,
+        withdrewAt: LocalDate,
+) : Aggregate() {
+    override val id: UUID = UUID.randomUUID()
+    private var title = title
+    private var content = content
+    private var publishedAt = publishedAt
+    private var withdrewAt = withdrewAt
+    override val domainEvents: List<DomainEvent> = mutableListOf()
+
+    companion object {
+        fun create(
+                title: String,
+                content: String,
+                comments: List<Comment>,
+                publishedAt: LocalDate,
+                withdrewAt: LocalDate,
+        ): Article {
+            return Article(
+                    UUID.randomUUID(),
+                    title,
+                    content,
+                    comments,
+                    publishedAt,
+                    withdrewAt,
+            )
+        }
+
+        fun reConstruct(
+                id: UUID,
+                title: String,
+                content: String,
+                comments: List<Comment>,
+                publishedAt: LocalDate,
+                withdrewAt: LocalDate,
+        ): Article {
+            return Article(
+                    id,
+                    title,
+                    content,
+                    comments,
+                    publishedAt,
+                    withdrewAt,
+            )
+        }
+    }
+
+    fun changeTitle(title: String) {
+        this.title = title
+    }
+
+    fun changeContent(content: String) {
+       this.content = content
+    }
 }
