9020c0c6a9d3dc2ba5c344f4eb925ab7be582a61
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-04-04 21:14:17 +0900

    update author

--- src/main/kotlin/com/example/kotlinSampleApp/domain/model/author/Author.kt
+++ src/main/kotlin/com/example/kotlinSampleApp/domain/model/author/Author.kt
@@ -1,4 +1,24 @@
 package com.example.kotlinSampleApp.domain.model.author
 
-class Author {
+import com.example.kotlinSampleApp.domain.event.DomainEvent
+import java.util.*
+import com.example.kotlinSampleApp.domain.model.Aggregate
+
+class Author private constructor(
+        id: UUID,
+        name: String,
+        articles: List<UUID>
+): Aggregate() {
+    override val id: UUID = UUID.randomUUID()
+    override val domainEvents: List<DomainEvent> = mutableListOf()
+
+    companion object {
+        fun create(name: String): Author {
+            return Author(
+                    UUID.randomUUID(),
+                    name,
+                    mutableListOf(),
+            )
+        }
+    }
 }
