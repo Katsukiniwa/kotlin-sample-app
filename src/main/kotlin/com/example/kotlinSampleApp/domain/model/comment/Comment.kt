35667cb1680361b322b30a7d3ca8a2888da5c0d2
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-04-05 23:27:40 +0900

    update comment

--- src/main/kotlin/com/example/kotlinSampleApp/domain/model/comment/Comment.kt
+++ src/main/kotlin/com/example/kotlinSampleApp/domain/model/comment/Comment.kt
@@ -1,4 +1,25 @@
 package com.example.kotlinSampleApp.domain.model.comment
 
-class Comment {
+import com.example.kotlinSampleApp.domain.event.DomainEvent
+import com.example.kotlinSampleApp.domain.model.Aggregate
+import com.example.kotlinSampleApp.domain.model.author.Author
+import java.util.*
+
+class Comment private constructor(
+        id: UUID,
+        body: String,
+        authorId: UUID,
+): Aggregate() {
+    override val id: UUID = UUID.randomUUID()
+    override val domainEvents: List<DomainEvent> = mutableListOf()
+
+    companion object {
+        fun create(body: String, authorId: UUID): Comment {
+            return Comment(
+                    UUID.randomUUID(),
+                    body,
+                    authorId,
+            )
+        }
+    }
 }
