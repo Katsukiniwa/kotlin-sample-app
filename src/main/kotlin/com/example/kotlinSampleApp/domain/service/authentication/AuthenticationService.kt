ff24c8fc80bfe33d651f609fa66cbe8b1575ef42
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-04-09 17:46:59 +0900

    define authentication service

--- src/main/kotlin/com/example/kotlinSampleApp/domain/service/authentication/AuthenticationService.kt
+++ src/main/kotlin/com/example/kotlinSampleApp/domain/service/authentication/AuthenticationService.kt
@@ -1,4 +1,6 @@
 package com.example.kotlinSampleApp.domain.service.authentication
 
 interface AuthenticationService {
+    fun generateHashedPassword(rawPassword: String): String
+    fun verifyPassword(rawPassword: String): Boolean
 }
